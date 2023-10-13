a03f193d0e084e0ecccbb35705f9a2d4c2601f03
Pablo QB <qb.pablo.2000@gmail.com>
2023-10-13 11:04:08 +0200

    Update train.py

--- train.py
+++ train.py
@@ -6,19 +6,19 @@ from torch import nn
 from torch import optim
 
 class Trainer:
-    def __init__(self, model, epochs, criterion, optim, lr, stopping_epochs,
-                 batches_per_epoch = None, use_cuda = True):
+    def __init__(self, model, epochs, criterion, optim, lr, stopping_batches,
+                 batch_logging_freq = None, use_cuda = True):
         self.model = model
         self.epochs = epochs
         self.criterion = criterion
         self.optim = optim(model.parameters(), lr)
         self.lr = lr
-        self.stopping_epochs = stopping_epochs
+        self.stopping_batches = stopping_batches 
         self.training_loss = []
         self.validation_loss = []
         self.record_loss = 9999
         self.epochs_since_loss_record = 0
-        self.batches_per_epoch = batches_per_epoch
+        self.batch_logging_freq = batch_logging_freq
         self.model_saved_batch = 0
 
         if not torch.cuda.is_available():
@@ -29,25 +29,25 @@ class Trainer:
 
 
     def train(self, trainloader, validloader):
-        if self.batches_per_epoch == None:
-            self.batches_per_epoch = len(trainloader)
+        if self.batch_logging_freq == None:
+            self.batch_logging_freq = len(trainloader)
 
-        for epoch in range(self.epochs):
+        for epoch in range(self.epochs): # Epoch loop
             trainiter = iter(trainloader)
-            for i in range(len(trainloader) // self.batches_per_epoch):
+            for i in range(len(trainloader) // self.batch_logging_freq): # Logging loop
                 self.model.train()
-                epoch_train_loss = self.train_epoch(trainiter)
+                epoch_train_loss = self.train_loop(trainiter)
                 self.training_loss.append(epoch_train_loss)
 
                 self.model.eval()
                 epoch_valid_loss = self.evaluate_model(validloader)
                 self.validation_loss.append(epoch_valid_loss)
 
-                n = epoch*self.batches_per_epoch + i
+                n = epoch*self.batch_logging_freq + i
                 stop, saved = self.early_stopping(epoch_valid_loss, n)
 
                 if n > 0:
-                    self.plot_losses(epoch, n) # This line clears the display
+                    self.plot_losses(epoch, n) # This line clears the display all printing should go after this
                 print(f"Epoch train loss: {epoch_train_loss}")
                 print(f"Epoch valid loss: {epoch_valid_loss}")
 
@@ -64,22 +64,22 @@ class Trainer:
         self.model.load_state_dict(state_dict)
 
     def plot_losses(self, epoch, n):
-        x = np.arange(n+1) / self.batches_per_epoch
+        x = np.arange(n+1) / self.batch_logging_freq
         plt.clf()
         plt.title(f"Loss up to epoch: {epoch+1}/{self.epochs}")
         plt.xlabel("Epoch")
         plt.ylabel("Loss")
         plt.plot(x, self.training_loss, label='Training Loss')
         plt.plot(x, self.validation_loss, label='Validation Loss')
-        plt.axvline(self.model_saved_batch / self.batches_per_epoch,
+        plt.axvline(self.model_saved_batch / self.batch_logging_freq,
                     ls=':', lw=0.5, color='grey', label = 'Saved model')
         plt.legend()
         display.clear_output(wait=True)
         display.display(plt.gcf())
 
-    def train_epoch(self, trainiter):
+    def train_loop(self, trainiter):
         running_loss = 0.
-        for _ in range(self.batches_per_epoch):
+        for _ in range(self.batch_logging_freq):
             x, y = next(trainiter)
             x, y = x.to(self.device), y.to(self.device)
             self.optim.zero_grad()
@@ -88,7 +88,7 @@ class Trainer:
             running_loss += loss.item()
             loss.backward()
             self.optim.step()
-        return running_loss / self.batches_per_epoch
+        return running_loss / self.batch_logging_freq
 
     def evaluate_model(self, validloader):
         epoch_valid_loss = 0.
@@ -112,6 +112,6 @@ class Trainer:
           else:
               self.epochs_since_loss_record += 1
 
-          if self.epochs_since_loss_record > self.stopping_epochs:
+          if self.epochs_since_loss_record > self.stopping_batches:
               return True, saved
           return False, saved
